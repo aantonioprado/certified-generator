a9537a1e5d52a830a45374deecf034fa183ca514
Antônio Prado <antonioeliaspr@gmail.com>
2025-08-05 01:02:45 -0300

    style: standardize language and fix typos across codebase

--- src/app/_components/item-certified/item-certified.ts
+++ src/app/_components/item-certified/item-certified.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { SecondaryButton } from '../secondary-button/secondary-button';
 
 @Component({
-  selector: 'app-item-certificate',
+  selector: 'app-item-certified',
   imports: [SecondaryButton],
-  templateUrl: './item-certificate.html',
-  styleUrl: './item-certificate.css'
+  templateUrl: './item-certified.html',
+  styleUrl: './item-certified.css'
 })
-export class ItemCertificate {
+export class ItemCertified {
 
 }
--- src/app/app.ts
+++ src/app/app.ts
@@ -4,15 +4,15 @@ import { CommonModule } from '@angular/common';
 import { Navbar } from './_components/navbar/navbar';
 import { PrimaryButton } from './_components/primary-button/primary-button';
 import { SecondaryButton } from './_components/secondary-button/secondary-button';
-import { ItemCertificate } from './_components/item-certificate/item-certificate';
+import { ItemCertified } from './_components/item-certified/item-certified';
 import { BaseUi } from './_components/base-ui/base-ui';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertificate, BaseUi],
+  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertified, BaseUi],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('gerador-certificado');
+  protected readonly title = signal('certified-generator');
 }
