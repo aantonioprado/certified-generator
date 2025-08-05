17f07150d9b8ebf992c16173d5f9c2440a19d9f7
Antônio Prado <antonioeliaspr@gmail.com>
2025-08-05 00:53:14 -0300

    feat: update app files

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Navbar } from './_components/navbar/navbar';
 import { CommonModule } from '@angular/common';
+import { Navbar } from './_components/navbar/navbar';
+import { PrimaryButton } from './_components/primary-button/primary-button';
+import { SecondaryButton } from './_components/secondary-button/secondary-button';
+import { ItemCertificate } from './_components/item-certificate/item-certificate';
+import { BaseUi } from './_components/base-ui/base-ui';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, CommonModule],
+  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertificate, BaseUi],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
