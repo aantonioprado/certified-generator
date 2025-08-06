a17333a8fe83caf9fab6649fcb1f46ad9b7714a1
Antônio Prado <antonioeliaspr@gmail.com>
2025-08-06 19:38:30 -0300

    feat: add routes in btn

--- src/app/_components/item-certified/item-certified.ts
+++ src/app/_components/item-certified/item-certified.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { SecondaryButton } from '../secondary-button/secondary-button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-item-certified',
-  imports: [SecondaryButton],
+  imports: [SecondaryButton, RouterLink],
   templateUrl: './item-certified.html',
   styleUrl: './item-certified.css'
 })
 export class ItemCertified {
-
+  id: string = "6";
 }
--- src/app/_components/navbar/navbar.ts
+++ src/app/_components/navbar/navbar.ts
@@ -1,17 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
-export class Navbar implements OnInit {
-  ngOnInit(): void {
-    this.message();
-  }
-
-  message() {
-    console.log("My component navbar is loaded (inside a function)");
-  }
-}
+export class Navbar {}
