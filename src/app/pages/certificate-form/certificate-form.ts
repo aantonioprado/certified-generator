9525fba889634c1b26fcab8275f0008177acced0
Antônio Prado <antonioeliaspr@gmail.com>
2025-08-06 20:22:11 -0300

    feat: add verify fields in form and adjusting styles

--- src/app/pages/certificate-form/certificate-form.ts
+++ src/app/pages/certificate-form/certificate-form.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { PrimaryButton } from '../../_components/primary-button/primary-button';
 import { SecondaryButton } from '../../_components/secondary-button/secondary-button';
+import { FormsModule, NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-certificate-form',
-  imports: [PrimaryButton, SecondaryButton],
+  imports: [PrimaryButton, SecondaryButton, FormsModule, CommonModule],
   templateUrl: './certificate-form.html',
   styleUrl: './certificate-form.css'
 })
 export class CertificateForm {
+  name: string = '';
+  activity: string = '';
+  activitys: string[] = ['aaa', 'aaaa'];
 
+  fieldEmpty(control: NgModel) {
+    return control.invalid && control.touched
+  }
 }
