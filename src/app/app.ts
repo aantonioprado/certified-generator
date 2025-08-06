922f24356f2be17931da452f73a514849ad445d5
Antônio Prado <antonioeliaspr@gmail.com>
2025-08-06 17:29:50 -0300

    feat: add page certicates

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,14 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Navbar } from './_components/navbar/navbar';
-import { PrimaryButton } from './_components/primary-button/primary-button';
-import { SecondaryButton } from './_components/secondary-button/secondary-button';
-import { ItemCertified } from './_components/item-certified/item-certified';
 import { BaseUi } from './_components/base-ui/base-ui';
+import { Certificates } from './pages/certificates/certificates';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertified, BaseUi],
+  imports: [Navbar, CommonModule, BaseUi, Certificates],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
