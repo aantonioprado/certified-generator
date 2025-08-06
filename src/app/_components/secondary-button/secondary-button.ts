80ff8a95ce1091abca40719e0c2676ef2f32da83
Antônio Prado <antonioeliaspr@gmail.com>
2025-08-06 20:48:35 -0300

    feat: add make props interactive

--- src/app/_components/secondary-button/secondary-button.ts
+++ src/app/_components/secondary-button/secondary-button.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-secondary-button',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './secondary-button.html',
   styleUrl: './secondary-button.css'
 })
 export class SecondaryButton {
-
+  @Input() text: string = '';
+  @Input() icon: string = '';
+  @Input() disabled: boolean = false;
 }
