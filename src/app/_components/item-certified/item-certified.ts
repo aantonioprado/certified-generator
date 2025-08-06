cb97debbd93c5a01d0b2bb5c9b0dd699d396358b
Antônio Prado <antonioeliaspr@gmail.com>
2025-08-06 19:45:44 -0300

    refactor: replace [routerLink] with (click) event

--- src/app/_components/item-certified/item-certified.ts
+++ src/app/_components/item-certified/item-certified.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { SecondaryButton } from '../secondary-button/secondary-button';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-item-certified',
-  imports: [SecondaryButton, RouterLink],
+  imports: [SecondaryButton],
   templateUrl: './item-certified.html',
   styleUrl: './item-certified.css'
 })
 export class ItemCertified {
   id: string = "6";
+
+  constructor(private router: Router) {}
+
+  redirectCertificate() {
+      this.router.navigate(['/certificados', 2])
+  }
 }
