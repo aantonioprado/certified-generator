d3ff9fa268aa2ce25f2b3c7581511ccdae30e058
Antônio Prado <antonioeliaspr@gmail.com>
2025-08-06 22:21:00 -0300

    feat: load certificate by id from route params

--- src/app/pages/certificate/certificate.ts
+++ src/app/pages/certificate/certificate.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { SecondaryButton } from '../../_components/secondary-button/secondary-button';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { certificateService } from '../../_services/certificate';
+import { Certificate } from '../../interfaces/certificate';
 
 @Component({
   selector: 'app-certificate',
@@ -9,5 +11,15 @@ import { RouterLink } from '@angular/router';
   styleUrl: './certificate.css'
 })
 export class CertificateComponent {
+  id: string | null = null;
+  certificate: Certificate | undefined;
 
+  constructor(private certificateService: certificateService, private route: ActivatedRoute){}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.id = params.get('id');
+      this.certificate = this.certificateService.certificates.find(item => item.id === this.id);
+    });
+  }
 }
