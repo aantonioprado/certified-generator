bbb7b4ebd49627e8ffc08a01ab20e2c60827e130
Antônio Prado <antonioeliaspr@gmail.com>
2025-08-06 21:27:34 -0300

    feat: create service certificate

---
+++ src/app/_services/certificate.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Certificate } from '../interfaces/certificate';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class certificateService {
+  certificates: Certificate[] = [];
+
+  addCertificate(certificate: Certificate) {
+    this.certificates.push(certificate);
+  }
+}
