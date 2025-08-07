c975652eca1256e7e23db343c95189e92999f55d
Antônio Prado <antonioeliaspr@gmail.com>
2025-08-06 21:09:35 -0300

    refactor: using interface in values

--- src/app/pages/certificate-form/certificate-form.ts
+++ src/app/pages/certificate-form/certificate-form.ts
@@ -3,6 +3,7 @@ import { PrimaryButton } from '../../_components/primary-button/primary-button';
 import { SecondaryButton } from '../../_components/secondary-button/secondary-button';
 import { FormsModule, NgModel } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Certificate } from '../../interfaces/certificate';
 
 @Component({
   selector: 'app-certificate-form',
@@ -11,24 +12,33 @@ import { CommonModule } from '@angular/common';
   styleUrl: './certificate-form.css'
 })
 export class CertificateForm {
-  name: string = '';
-  activity: string = '';
-  activities: string[] = [];
+  certificate: Certificate = {
+    name: "",
+    activities: []
+  };
+  activity: string = "";
+
 
   fieldEmpty(control: NgModel) {
     return control.invalid && control.touched;
   }
 
   formValid() {
-    return this.activities.length > 0 && this.name.length > 0;
+    return this.certificate.activities.length > 0 && this.certificate.name.length > 0;
   }
 
   addActivity() {
-    this.activities.push(this.activity);
+    this.certificate.activities.push(this.activity);
     this.activity = '';
   }
 
   removeActivity(index: number) {
-    this.activities.splice(index, 1);
+    this.certificate.activities.splice(index, 1);
+  }
+
+  submit() {
+    if(!this.formValid) return;
+
+    
   }
 }
