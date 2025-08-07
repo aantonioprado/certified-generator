8f5c13226e0a9bfa97207425c241871e8100c653
Antônio Prado <antonioeliaspr@gmail.com>
2025-08-06 21:28:30 -0300

    feat: add save certificates in services

--- src/app/pages/certificate-form/certificate-form.ts
+++ src/app/pages/certificate-form/certificate-form.ts
@@ -4,6 +4,7 @@ import { SecondaryButton } from '../../_components/secondary-button/secondary-bu
 import { FormsModule, NgModel } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Certificate } from '../../interfaces/certificate';
+import { certificateService } from '../../_services/certificate';
 
 @Component({
   selector: 'app-certificate-form',
@@ -12,9 +13,12 @@ import { Certificate } from '../../interfaces/certificate';
   styleUrl: './certificate-form.css'
 })
 export class CertificateForm {
+  constructor(private certificateService: certificateService) {}
+
   certificate: Certificate = {
     name: "",
-    activities: []
+    activities: [],
+    dateIssue: ""
   };
   activity: string = "";
 
@@ -38,7 +42,18 @@ export class CertificateForm {
 
   submit() {
     if(!this.formValid) return;
+    this.certificate.dateIssue = this.getCurrentDate();
+    this.certificateService.addCertificate(this.certificate)
 
-    
+  }
+
+  getCurrentDate() {
+    let date = new Date();
+    let [day, month, year] = [
+      String(date.getDate()).padStart(2, '0'),
+      String(date.getMonth() + 1).padStart(2, '0'),
+      date.getFullYear()
+    ];
+    return `${day}/${month}/${year}`;
   }
 }
